f76db8788348da0c2f31f48c09013570b8bfb7df
Hosalli <vasudev-hrms@users.noreply.github.com>
2016-06-29 18:17:32 +0530

    test

---
+++ scripts/SampleScript.js
@@ -0,0 +1,18 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var data=
+
+[
+  ['Country', 'Popularity'],
+  ['Germany', 200],
+  ['United States', 300],
+  ['Brazil', 400],
+  ['Canada', 500],
+  ['France', 600],
+  ['RU', 700]
+]
+
+return data;			
